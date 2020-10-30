f44299ed7f9e9083e689a2a06163fbda518c4ef3
jellyfish <johnrclifford@gmail.com>
2020-10-30 17:42:38 -0500

    updated readMe; created database schema and seed; created json packages; created connection.js

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mega Meat', true);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mac Patty', true);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Lean Bean', false);
+
